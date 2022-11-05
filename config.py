f7c630919711aaa5bdcce42202363e34a852ce30
Xuanrun Wang <810158407@qq.com>
2022-11-05 16:45:14 +0800

    修改cookie存储方式

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
+with open('cookie.txt', 'r') as fin:
+    cookie = fin.read()
+
 headers = {
-    'cookie': '',
+    'cookie': cookie,
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
